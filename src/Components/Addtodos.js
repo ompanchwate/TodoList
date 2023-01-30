d2320ca87aa54f910bf2780e4dc8aca2d4f8b5b9
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-01-30 12:55:52 +0530

    Done

--- src/Components/Addtodos.js
+++ src/Components/Addtodos.js
@@ -19,12 +19,12 @@ export const Addtodos = ({ addtodo }) => {
     return (
         <>
             <h1 className='font-bold text-xl lg:text-2xl text-center text-blue-600 mt-2'>Add Todo</h1>
-            <form className=' mx-10 xl:mx-64 text-center xl:text-left' onSubmit={submit}>
-                <div className='space-y-5 '>
+            <form className=' mx-5 xl:mx-64 text-center xl:text-left' onSubmit={submit}>
+                <div className='lg:space-y-5 space-y-1'>
                     <label className='font-bold p-1 mr-5'>Enter Title</label>
                     <input type="text" value={title} placeholder="Enter Title" className='border-2 border-gray-400 rounded-lg p-1 sm:w-[30rem] xl:w-[50rem]' onChange={(e) => settitle(e.target.value)} />
                     <br />
-                    <label className='font-bold p-1 mr-3'>Description</label>
+                    <label className='font-bold p-1 mr-3  '>Description</label>
                     <input type="text" value={desc} placeholder="Description" className='border-2 border-gray-400 rounded-lg p-1 sm:w-[30rem] xl:w-[50rem]' onChange={(e) => setdesc(e.target.value)} />
                     <br />
                     <button type='submit' className='p-1  bg-green-600 border-2 text-white font-semibold border-green-600 rounded-lg text-left px-5 cursor-pointer hover:bg-transparent hover:text-black'>Submit</button>
